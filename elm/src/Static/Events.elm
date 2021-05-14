391a7c3ec55e571c501452c5dae98fdd419d69b2
kiriakos <kiriakos@kind.software>
2021-05-14 22:24:50 +0200

    Alonzo SPO Testnet

--- elm/src/Static/Events.elm
+++ elm/src/Static/Events.elm
@@ -12,7 +12,10 @@ milestones =
     , ME.Milestone <| ME.TheMilestone "Native Assets" (Just 1614635091) True mairyUrl <| Just "NFTs and other tokens, natively on the Cardano ledger!"
     , ME.Milestone <| ME.TheMilestone "d=0" (Just 1617227091) True Nothing <| Just "Full decentralization, the legacy BFT nodes can be taken completely offline, the community assumes complete responsibility for the operation of the mainnet."
     , ME.Milestone <| ME.TheMilestone "Alonzo Node" (Just 1618523091) True Nothing <| Just "In mid April the functionality for executing smart contracts inside the cardano-node component reaches maturity and can be used in the various testnets that will be following. Now an internal testnet can be launched to prepare for the Alonzo Pioneers era."
-    , ME.Milestone <| ME.TheMilestone "Alonzo Pioneers Testnet" (Just 1619819091) False Nothing <| Just "The first public testnet for Goguen. The first batch of around 1k external Alonzo pioneers will get access to the Pioneers Testnet where the functionalities of Alonzo will be tested in close collaboration with IOHK."
+    , ME.Milestone <| ME.TheMilestone "Alonzo Internal Testnet" (Just 1619819091) True Nothing <| Just "An IOHK internal testnet for Goguen. The first batch of developers of the IOHK Core Engineering Team take the alonzo node for a spin internally"
+    , ME.Milestone <| ME.TheMilestone "Alonzo SPO Testnet" (Just 1622307600) True alonzoSpoNetUrl <| Just "Private testnet for Goguen. Select SPOs and IOHK partners test the first version of the alonzo node."
+
+    --, ME.Milestone <| ME.TheMilestone "Alonzo Pioneers Testnet" (Just ) False Nothing <| Just "The first public testnet for Goguen. The first batch of around 1k external Alonzo pioneers will get access to the Pioneers Testnet where the functionalities of Alonzo will be tested in close collaboration with IOHK."
     , ME.Milestone <| ME.TheMilestone "Alonzo Feature Freeze" (Just 1625003091) False Nothing <| Just "We lock down the code, this is the big one! After 2+ months of rigorous testing in the private and pioneers testnets the codebase will have been ironed out. Now we freeze the codebase and run stress tests on the networks one more time to ensure everyting is rock sollid because next comes..."
     , ME.Milestone <| ME.TheMilestone "Goguen" (Just 1627595091) False goguenUrl <| Just "The Alonzo HFC. Full Smart Contract functionality, Haskell and formally verified DAOs, ERC-20 importers on the Cardano mainnet"
     , ME.Milestone <| ME.TheMilestone "Pergamon" Nothing True pergamonUrl <| Just "An experiment about human collaboration and NFTs"
@@ -36,6 +39,11 @@ streams =
 
 
 --, ME.Stream <| ME.TheStream "Running Stream" "a running one" "en" "https://do.co" 1616172575 7200
+
+
+alonzoSpoNetUrl : Maybe String
+alonzoSpoNetUrl =
+    Just "https://youtu.be/5mPC4uLMdEw?t=456"
 
 
 pergamonUrl : Maybe String
